fc7e9eb5ffa7afb0ef193bbe73a676f69d36f8bb
avi <tenzer@clara.co.uk>
2019-10-03 18:26:01 +0200

    added transaction update

--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -46,4 +46,22 @@ describe('Transaction', () => {
 
     });
 
+    describe(' an updating a transaction', () => {
+        let nextAmount, nextRecipient;
+        beforeEach( () => {
+            nextAmount = 20,
+                nextRecipient = 'n3xt-4ddr355';
+            transaction = transaction.update(wallet, nextRecipient, nextAmount);
+        });
+
+        it(`substracts the next amount from the sender's output`, () => {
+            expect(transaction.outputs.find(output => output.address === wallet.publicKey).amount).toEqual(wallet.balance - amount - nextAmount);
+        });
+
+        it(`outputs an amount for the next recipient`, () => {
+            expect(transaction.outputs.find(output => output.address === nextRecipient).amount).toEqual(nextAmount);
+        });
+
+    });
+
 })
