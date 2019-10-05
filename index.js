8b90ebef342551fb5eb16210786092cc81acfb5f
avi <tenzer@clara.co.uk>
2019-10-05 18:35:47 +0200

    added get/post transactions

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain/blockchain');
 const P2pServer = require('./p2p-server');
+const Wallet = require('../blockchain/wallet/index');
+const TransactionPool = require('../blockchain/wallet/transaction-pool');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
+const wallet = new Wallet();
+const tp = new TransactionPool();
 const p2pserver = new P2pServer(bc);
 
 app.use(bodyParser.json());
@@ -23,6 +27,21 @@ app.post('/mine', (req,res) => {
 
    res.redirect('/blocks');
 })
+
+app.get('/transactions', (req, res) => {
+   res.json(tp.transactions);
+});
+
+app.post('/transactions', (req,res) => {
+
+   const { recipient, amount} = req.body;
+   const transaction = wallet.createTransaction(recipient, amount, tp);
+
+   res.redirect('/transactions');
+
+})
+
+
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
 p2pserver.listen();
