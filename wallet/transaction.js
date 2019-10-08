8e32b991c96fe92680143f2c83290d210c2e1ff3
avi <tenzer@clara.co.uk>
2019-10-08 15:00:20 +0200

    added blockchain wallet

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -1,4 +1,6 @@
 const ChainUtil = require('../chain-util');
+const { MINING_REWARD } = require('../config');
+
 
 class Transaction {
     constructor(){
@@ -21,6 +23,13 @@ class Transaction {
         return this;
     }
 
+    static transactionWithOutputs(senderWallet, outputs){
+        const transaction = new this();
+        transaction.outputs.push(...outputs);
+        Transaction.signTransaction(transaction, senderWallet);
+        return transaction;
+    }
+
     static newTransaction(senderWallet, recipient, amount ){
 
         const transaction = new this();
@@ -29,15 +38,18 @@ class Transaction {
             return;
         }
 
-        transaction.outputs.push(... [
+        return Transaction.transactionWithOutputs(senderWallet, [
             { amount: senderWallet.balance - amount,
-              address: senderWallet.publicKey
+                address: senderWallet.publicKey
             },
             {amount, address: recipient}
-        ]);
-        Transaction.signTransaction(transaction, senderWallet);
+        ])
+    }
 
-        return transaction;
+    static rewardTransaction(minerWallet, blockchainWallet){
+        return Transaction.transactionWithOutputs(blockchainWallet, [{
+            amount: MINING_REWARD, address: minerWallet.publicKey
+        }])
     }
 
     static signTransaction(transaction, senderWallet){
@@ -51,6 +63,12 @@ class Transaction {
 
     static verifyTransaction(transaction){
         return ChainUtil.verifySignature(transaction.input.address, transaction.input.signature, ChainUtil.hash(transaction.outputs));
+    }
+
+    static blockchainWallet(){
+        const blockchainWallet = new this();
+        blockchainWallet.address - 'blockchain-wallet';
+        return blockchainWallet;
     }
 
 }
